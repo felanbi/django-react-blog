b6585fa4a029b1701161743c15a0bce91440b455
gitea <test@gitea.fr>
2024-10-06 03:02:36 +0200

    Add swagger docs for admin users only

--- back/core/settings.py
+++ back/core/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-        
+
+    'drf_yasg',
+    'rest_framework',
+
     'accounts',
     'api',
     'contact'
