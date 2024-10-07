17ddfba6a0cdf9a4555ba9f0126e9366b4939a06
gitea <test@gitea.fr>
2024-10-07 17:00:09 +0200

    move swagger inside API project and add jwt token management

--- back/core/settings.py
+++ back/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'drf_yasg',
     'rest_framework',
+    'rest_framework_simplejwt',
 
     'accounts',
     'api',
--- back/core/urls.py
+++ back/core/urls.py
@@ -2,25 +2,12 @@ from django.contrib import admin
 from django.conf.urls import handler404, handler500, handler403
 from django.shortcuts import redirect
 from django.urls import path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title = "Snippets API",
-      default_version = 'v1',
-      description = "Swagger for blog project",
-   ),
-   public = True,
-   permission_classes = (permissions.IsAdminUser,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
-    path('docs/', schema_view.with_ui('swagger', cache_timeout = 0), name = 'schema-swagger-ui'),
     path('contact/', include('contact.urls')),
+	 path('api/', include('api.urls'))
 ]
 
 def default_error_handler(*args, **kwargs):
