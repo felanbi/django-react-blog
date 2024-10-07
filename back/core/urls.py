400f5a2e21f0e148b7b8ce1bff92537945917932
gitea <test@gitea.fr>
2024-10-07 18:23:51 +0200

    Add settings to manage images files

--- back/core/urls.py
+++ back/core/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
-from django.conf.urls import handler404, handler500, handler403
 from django.shortcuts import redirect
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import handler404, handler500, handler403
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('contact/', include('contact.urls')),
-	 path('api/', include('api.urls'))
-]
+	path('api/', include('api.urls'))
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 def default_error_handler(*args, **kwargs):
 	return redirect('login')
